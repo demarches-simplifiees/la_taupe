3299496488c69c39120cbed500c6b8a836510213
simon lehericey <mail@simon.lehericey.net>
2024-02-12 11:53:41 +0100

    fetch_datamatrix now takes a dynamic image

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use la_taupe::{datamatrix::fetch_datamatrix, twoddoc::parse};
+use la_taupe::{datamatrix::fetch_datamatrix, file_utils::file_to_img, twoddoc::parse};
 
 fn main() {
-    let datamatrix = fetch_datamatrix("tests/fixtures/2ddoc/justificatif_de_domicile.png");
+    let img = file_to_img("tests/fixtures/2ddoc/justificatif_de_domicile.pdf");
+
+    let datamatrix = fetch_datamatrix(img);
 
     if let Some(datamatrix) = datamatrix {
         let ddoc = parse(&datamatrix);
