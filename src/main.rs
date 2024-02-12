a6a48a3882f9ce98753f3d234fb4293b7513f242
simon lehericey <mail@simon.lehericey.net>
2024-02-12 14:16:44 +0100

    add path argument to the cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::env::args;
+
 use la_taupe::{datamatrix::fetch_datamatrix, file_utils::file_to_img, twoddoc::parse};
 
 fn main() {
-    let img = file_to_img("tests/fixtures/2ddoc/justificatif_de_domicile.pdf");
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path_to_file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_path = &args[1];
+
+    let img = file_to_img(file_path);
 
     let datamatrix = fetch_datamatrix(img);
 
