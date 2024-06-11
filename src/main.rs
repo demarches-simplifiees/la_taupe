7ef76dffbdd4bd018300e7eb60800383f4de3282
simon lehericey <mail@simon.lehericey.net>
2024-06-11 16:32:30 +0200

    add --version --help flags to cli

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ fn main() {
 
     let args: Vec<String> = args().collect();
 
+    if args.contains(&String::from("--help")) {
+        println!("La taupe: a tool to analyze files");
+        println!("--version to print the version");
+        println!("la_taupe file_path to analyze a file");
+        println!("la_taupe to start the server");
+        std::process::exit(0);
+    }
+
+    if args.contains(&String::from("--version")) {
+        println!("Version: {}", env!("GIT_HASH"));
+        std::process::exit(0);
+    }
+
     if args.len() == 1 {
         let _ = server::main();
     } else {
