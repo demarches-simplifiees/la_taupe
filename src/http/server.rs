be3d39a8f70a6248209b7410fbb62a53aaf0b6a3
simon lehericey <mail@simon.lehericey.net>
2024-06-14 15:07:50 +0200

    use LA_TAUPE_ADDRESS to bind to specific address

--- src/http/server.rs
+++ src/http/server.rs
@@ -1,5 +1,8 @@
+use std::env;
+
 use super::analyze;
 use actix_web::{middleware, App, HttpServer};
+use std::net::{SocketAddr, ToSocketAddrs};
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
@@ -8,7 +11,14 @@ pub async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(analyze::analyze)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(binding_address())?
     .run()
     .await
+}
+
+pub fn binding_address() -> SocketAddr {
+    let address = env::var("LA_TAUPE_ADDRESS")
+        .unwrap_or_else(|_| "127.0.0.1:8080".to_string())
+        .to_string();
+    address.to_socket_addrs().unwrap().next().unwrap()
 }
