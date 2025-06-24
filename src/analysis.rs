e453960858f2d3bab28c5ef2a3e32910a55b6fe1
simon lehericey <mail@simon.lehericey.net>
2025-06-24 16:55:54 +0200

    image extracted from pdf also checks tesseract then ocrs

--- src/analysis.rs
+++ src/analysis.rs
@@ -2,15 +2,15 @@ use std::path::Path;
 
 use serde::{Deserialize, Serialize};
 
+use crate::file_utils::pdf_to_img_bytes;
+use crate::ocr::Ocr::{Ocrs, Tesseract};
+use crate::rib::Rib;
 use crate::{
     datamatrix::fetch_datamatrix,
     file_utils::{bytes_to_img, pdf_bytes_to_string},
     ocr::image_bytes_to_string,
     twoddoc::{ddoc::Ddoc, parse},
 };
-use crate::ocr::Ocr::{Tesseract, Ocrs};
-
-use crate::rib::Rib;
 
 #[derive(Deserialize, Serialize)]
 pub struct Analysis {
@@ -37,16 +37,16 @@ fn vec_to_rib(content: Vec<u8>) -> Result<Rib, String> {
     let filetype = tree_magic_mini::from_u8(&content);
 
     if filetype == "application/pdf" {
-        let string_rib = pdf_bytes_to_string(content)?;
-        Rib::try_from(string_rib)
-    } else if filetype == "image/png" || filetype == "image/jpeg" {
-        let string_rib_tesseract = image_bytes_to_string(content.clone(), Tesseract);
-        if let Ok(rib) = Rib::try_from(string_rib_tesseract) {
-            return Ok(rib);
-        }
+        let string_rib = pdf_bytes_to_string(content.clone());
 
-        let string_rib_ocrs = image_bytes_to_string(content, Ocrs);
-        Rib::try_from(string_rib_ocrs)
+        if !string_rib.trim().is_empty() {
+            Rib::try_from(string_rib)
+        } else {
+            let img = pdf_to_img_bytes(content);
+            image_to_rib(img)
+        }
+    } else if filetype == "image/png" || filetype == "image/jpeg" {
+        image_to_rib(content)
     } else if filetype == "text/plain" {
         let string_rib = String::from_utf8(content)
             .map_err(|_| "Failed to convert bytes to string".to_string())?;
@@ -54,6 +54,16 @@ fn vec_to_rib(content: Vec<u8>) -> Result<Rib, String> {
     } else {
         Err(format!("Unsupported file type: {}", filetype))
     }
+}
+
+fn image_to_rib(content: Vec<u8>) -> Result<Rib, String> {
+    let string_rib_tesseract = image_bytes_to_string(content.clone(), Tesseract);
+    if let Ok(rib) = Rib::try_from(string_rib_tesseract) {
+        return Ok(rib);
+    }
+
+    let string_rib_ocrs = image_bytes_to_string(content, Ocrs);
+    Rib::try_from(string_rib_ocrs)
 }
 
 fn vec_to_ddoc(content: Vec<u8>) -> Result<Ddoc, String> {
